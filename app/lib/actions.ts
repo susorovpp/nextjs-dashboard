8cadb971b78b93b52739084ecc2e4508e970df92
Pavel Susorov <pavelsusorov@Pavels-MacBook-Pro-2.local>
2025-02-01 21:40:10 +0200

    fix eslint errors

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -51,6 +51,7 @@ export async function createInvoice(prevState: State, formData: FormData) {
       INSERT INTO invoices (customer_id, amount, status, date)
       VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
     `;
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
     } catch (error) {
         // If a database error occurs, return a more specific error.
         return {
@@ -87,7 +88,8 @@ export async function updateInvoice(id: string, prevState: State, formData: Form
         SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
         WHERE id = ${id}
       `;
-    } catch (error) {
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    } catch (e) {
         return { message: 'Database Error: Failed to Update Invoice.' };
     }
 
